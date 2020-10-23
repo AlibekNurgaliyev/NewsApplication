e8b4fcf1b84564fb053b599a9fb73acc0b5563eb
alibek.nurgaliyev <alibek.nurgaliyev@gmail.com>
2020-10-23 17:01:00 +0600

    init

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        //        const val API_KEY = "e65ee0938a2a43ebb15923b48faed18d"
+        const val API_KEY = "da7983030c5e45bb9158acba9b9ab322"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DEALAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
